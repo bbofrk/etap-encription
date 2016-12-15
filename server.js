791f336ba05979113487001a148cd6516120a1e7
bbofrk <mingliwanglau@gmail.com>
2016-12-15 22:28:18 +1300

    ready for heroku

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const port = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('hello express');
+  // res.send('hello express');
+  res.sendFile('public/help.html', {root: __dirname});
 });
 
-app.listen(3000);
+app.get('/message', (req, res) => {
+  res.sendFile('public/message.html', {root: __dirname});
+});
+
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`)
+});
